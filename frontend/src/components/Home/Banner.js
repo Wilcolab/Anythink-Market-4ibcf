2a53a0a59d144ca02c071cf543222907b8540631
Sharma Ketan <sharmaketann@gmail.com>
2022-08-16 22:45:55 +0530

    responsive issue resolved

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import logo from '../../imgs/logo.png'
+import React from "react";
+import logo from "../../imgs/logo.png";
 
 const Banner = () => {
   return (
-    <div className='banner text-white'>
-      <div className='container p-4 text-center'>
-        <img src={logo} alt='banner' className='w-100' />
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" className="w-100" />
         <div>
-          <span id='get-part'>A place to get</span>
+          <span id="get-part">A place to get</span>
           <span> the cool stuff.</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
