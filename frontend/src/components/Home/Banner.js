2165aad9f249163c702c4bddaa88ce78fd3f6323
Sharma Ketan <sharmaketann@gmail.com>
2022-08-17 18:48:37 +0530

    lint issue resolved

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react'
-import logo from '../../imgs/logo.png'
+import React, { useState } from "react";
+import logo from "../../imgs/logo.png";
 
 const Banner = ({ search, setSearch }) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
   return (
-    <div className='banner text-white'>
-      <div className='container p-4 text-center'>
-        <img src={logo} alt='banner' />
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
         <h2>
-          A place to{' '}
-          <span id='get-part' onClick={() => setVisible((i) => !i)}>
+          A place to{" "}
+          <span id="get-part" onClick={() => setVisible((i) => !i)}>
             get
           </span>
           <input
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            id='search-box'
-            placeholder='What is it that you truly desire?'
-            style={{ fontSize: 'small', display: visible ? undefined : 'none' }}
+            id="search-box"
+            placeholder="What is it that you truly desire?"
+            style={{ fontSize: "small", display: visible ? undefined : "none" }}
           />
           the cool stuff.
         </h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
